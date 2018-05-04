b45a266a1698dd40f25481032610407aa55618f8
Morgan Peck <MorganPeck@Morgan-Pecks-MacBook-Pro.local>
2018-05-03 23:26:36 -0500

    I have started to bbuild the shell of my application

---
+++ seed/product-seeder.js
@@ -0,0 +1,10 @@
+var Product = require ('../models/product');
+
+var products = [
+    new Product({
+        imagePath:"https://firebasestorage.googleapis.com/v0/b/shoppi-14d4e.appspot.com/o/shoe            .png?alt=media&token=e1c945a1-dbef-49a3-972c-2a131ef9a91d",
+        title:'Shoe',
+        description: 'Cool Shoe!! This is awesome to wear on your foot!',
+        price: 67.99
+}),
+];
