1c4580003853ab84b6f9d4a58eb84dd15a7e3bc3
Morgan Peck <MorganPeck@Morgan-Pecks-MacBook-Pro.local>
2018-05-05 16:23:32 -0500

    I have added successfully looped through and displayed my data to the splash page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var Product = require('../models/product');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'WD6' });
+  Product.find(function (err,docs) {
+
+      res.render('shop/index', { title: 'WD6', products: docs});
+  });
+
 });
 
 module.exports = router;
