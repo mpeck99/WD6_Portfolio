58402281e05f0b5ef5c05ef29da1a4da8892bba0
Morgan Peck <MorganPeck@Morgan-Pecks-MacBook-Pro.local>
2018-05-16 23:02:15 -0500

    validating user signup

--- app.js
+++ app.js
@@ -7,18 +7,28 @@ var expressHbs = require('express-handlebars');
 var indexRouter = require('./routes/index');
 var mongoose = require('mongoose');
 var app = express();
-
-//connneting to mongodb
+var session = require('express-session');
+var passport= require('passport');
+var flash = require('connect-flash');
+var bodyParser = require('body-parser');
+var validator = require('express-validator');
+// connecting to mongodb
 mongoose.connect('mongodb://localhost:27017/shopping');
-
+require('./config/passport');
 // view engine setup
 app.engine('.hbs',expressHbs({defaultLayout: 'layout',extname:'.hbs'}));
 app.set('view engine', '.hbs');
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:false }));
+app.use(validator());
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({secret: 'wd6secret', resave: false, saveUnitialized: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var csrf = require('csurf');
+var passport = require('passport');
+var csrfProtection = csrf();
 var Product = require('../models/product');
+router.use(csrfProtection);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,4 +15,18 @@ router.get('/', function(req, res, next) {
 
 });
 
+router.get('/user/signup',function (req,res,next) {
+  var messages = req.flash('error');
+  res.render('user/signup',{ csrfToken: req.csrfToken(),messages:messages,hasErrors:messages.length>0});
+});
+
+router.post('/user/signup',passport.authenticate('local.signup',{
+  successRedirect: '/user/profile',
+  failureRedirect: '/user/signup',
+  failureFlash: true
+}));
+
+router.get('/user/profile',function (req, res,next) {
+  res.render('user/profile');
+});
 module.exports = router;
